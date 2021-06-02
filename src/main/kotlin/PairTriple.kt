9c7a72579da47493e717145ab303bff3b6e2e3c5
lob <coffeescript@kakao.com>
2021-06-02 23:40:52 +0900

    Add Pair, Triple

---
+++ src/main/kotlin/PairTriple.kt
@@ -0,0 +1,17 @@
+
+fun main(args: Array<String>) {
+
+    val pair: Pair<Long, String> = Pair(1L, "Lob")
+    println("pair = $pair")
+
+    // fun <K, V> mapOf(pair: Pair<K, V>): Map<K, V>
+    // val map: Map<Long, String> = mapOf<Long, String>()
+    val map: Map<Long, String> = mapOf(1L to "Lob")
+    println("map = $map")
+
+    val map2 = emptyMap<Long, String>()
+    val map3 = mapOf<Long, String>()
+    println("${map2 == map3}")
+
+
+}
